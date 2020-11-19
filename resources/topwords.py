4fca35ad5965d46c1db8495246fbc01df415190b
kcapalar <62743064+kcapalar@users.noreply.github.com>
2020-11-19 15:22:05 +0800

    modified file location

--- resources/topwords.py
+++ resources/topwords.py
@@ -28,10 +28,10 @@ lemmatizer = WordNetLemmatizer()
 #All stopwords
 STOPWORDS = set(stopwords.words('english'))
 engtl_stop_words = []
-for words in open(r'./resources/English-Tagalog Stopwords.txt', 'r'):
+for words in open(r'https://raw.githubusercontent.com/kcapalar/verbatim-analytics/main/resources/English-Tagalog%20Stopwords.txt', 'r'):
     engtl_stop_words.append(words.strip().lower())
 
-with open(r'./resources/shortcuts_dict.txt', 'r') as s:
+with open(r'https://raw.githubusercontent.com/kcapalar/verbatim-analytics/main/resources/shortcuts_dict.txt', 'r') as s:
     data = s.read()
 shortcut = json.loads(data)
 
