2a18605cd260430c61fdf5af50f6fc6a707fb6d4
kcapalar <62743064+kcapalar@users.noreply.github.com>
2020-11-19 16:29:11 +0800

    Update topwords.py

--- resources/topwords.py
+++ resources/topwords.py
@@ -187,7 +187,6 @@ def load_topwords_page(data):
         #Unigram
             uni_df = load_topwords(data, ngram=(1,1))
             one_word = st.multiselect("Select words to remove: ", options=(uni_df["word"].values))
-	    st.markdown(get_table_download_link(uni_df, text='- Unigram (one-word)'), unsafe_allow_html=True)
             if len(one_word)>0:
                 mask = uni_df['word'].isin(one_word)
                 new_uni_df = uni_df[~mask]
@@ -197,7 +196,6 @@ def load_topwords_page(data):
         #Bigram
             bi_df = load_topwords(data, ngram=(2,2))
             two_word = st.multiselect("Select words to remove: ", options=(bi_df["word"].values))
-	    st.markdown(get_table_download_link(uni_df, text='- Bigram (two-word)'), unsafe_allow_html=True)
             if len(two_word)>0:
                 mask = bi_df['word'].isin(two_word)
                 new_bi_df = bi_df[~mask]
@@ -207,7 +205,6 @@ def load_topwords_page(data):
         #Trigram
             tri_df = load_topwords(data, ngram=(3,3))
             three_word = st.multiselect("Select words to remove: ", options=(tri_df["word"].values))
-	    st.markdown(get_table_download_link(uni_df, text='- Trigram (three-word)'), unsafe_allow_html=True)
             if len(three_word)>0:
                 mask = tri_df['word'].isin(three_word)
                 new_tri_df = tri_df[~mask]
